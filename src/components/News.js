f13a461af0e2c62e74b8a25fe095a2bfdc51da35
APURVA SINGH <singhapurva42@gmail.com>
2022-08-10 00:24:55 +0530

    Fetching API key from news API

--- src/components/News.js
+++ src/components/News.js
@@ -4,10 +4,20 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is news component
-        <NewsItem/>
-        <NewsItem/>
+      <div className="container my-3">
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+            <div className="col-md-4">
+                <NewsItem title="myTitle" description="mydescription"/>
+            </div>  
+            <div className="col-md-4">
+                <NewsItem title="myTitle" description="mydescription"/>
+            </div> 
+            <div className="col-md-4">
+                <NewsItem title="myTitle" description="mydescription"/>
+            </div> 
+            
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} = this.props;
     return (
       <div>
-        this is news item
+        <div class="card" style={{width: "18rem"}}>
+            <img src="https://www.nationalreview.com/wp-content/uploads/2022/08/Donald-Trump-30.jpg?fit=2057%2C1200" className="card-img-top" alt="..."/>
+            <div class="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
       </div>
     )
   }
